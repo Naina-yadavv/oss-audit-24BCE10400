9480ebaa7624f1395c3b609d54d4b58d8955ae9b
Naina Yadav <nainayadav1410@gmail.com>
2026-03-31 13:49:12 +0000

    updated script

--- script1.sh
+++ script1.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
+# Script 1: System Identity Report
+# Author: Naina Yadav
 
-echo "System Info"
-echo "User: $(whoami)"
-echo "Kernel: $(uname -r)"
-echo "Uptime: $(uptime -p)"
+# Variables
+STUDENT_NAME="Naina Yadav"
+SOFTWARE_CHOICE="Git"
+
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME=$(uptime -p)
+DATE=$(date)
+
+# Get distro name
+DISTRO=$(cat /etc/os-release | grep PRETTY_NAME | cut -d= -f2 | tr -d '"')
+
+echo "====================================="
+echo " Open Source Audit — $STUDENT_NAME"
+echo "====================================="
+echo "Software Chosen : $SOFTWARE_CHOICE"
+echo "Distro          : $DISTRO"
+echo "Kernel          : $KERNEL"
+echo "User            : $USER_NAME"
+echo "Uptime          : $UPTIME"
+echo "Date & Time     : $DATE"
+echo "License         : Linux is mostly GPL licensed"
+echo "====================================="
